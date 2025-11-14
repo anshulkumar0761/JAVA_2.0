5564c8763957b8e1d7ebe18a1a40ecdb4388858e
Anshul Kumar <anshulkumargond@gmail.com>
2025-11-15 00:56:32 +0530

    IF ELSE

---
+++ Lect2/src/forloop.java
@@ -0,0 +1,10 @@
+class Loop {
+    public static void main (String[] args){
+        String str = "nitin";
+        for (int i=str.length()-1 ; i>=0; i--){
+          System.out.print(str.charAt(i));
+
+    }
+
+    }
+}
---
+++ Lect2/src/scan.java
@@ -0,0 +1,32 @@
+import java.util.Scanner ;
+
+public class scan {
+    public static void main  ( String[] args )
+    {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("enter a word ");
+//        String c = sc.next();
+//        System.out.println("enter a number ");
+//        int a = sc.nextInt();
+//        System.out.println("enter a double");
+//        double b = sc.nextDouble();
+//        System.out.println("enter a string " );
+//        String d = sc.next();
+//
+//
+//
+//        System.out.println("word "+ c);
+//        System.out.println("Integer "+ a);
+//        System.out.println("Integer "+ b);
+//        System.out.println("String "+ d);
+
+
+                                           // type casting
+          short a = 128 ;
+          int b = a;
+          System.out.println(b);
+
+
+
+    }
+}
