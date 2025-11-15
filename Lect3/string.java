ca8501a469926af55db72fe5b6c4f66e38885e28
Anshul Kumar <anshulkumargond@gmail.com>
2025-11-15 17:57:47 +0530

    STRING

---
+++ Lect3/string.java
@@ -0,0 +1,13 @@
+package Lect3;
+
+public class string {
+    public static void main (String[] args){
+        String str = " Hello ";
+        System.out.println(str.length());
+        System.out.println(str.charAt(3));
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+        System.out.println(str.trim());
+        System.out.println(str.substring(2));
+    }
+}
